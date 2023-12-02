03a1ff8ba7ee583dcf534baf9c9c7fd03523ad7d
Maarten Staa <maarten@staa.dev>
2023-12-02 17:44:14 +0100

    chore: refactor day 1 to prepare for part 2

--- crates/day1/src/main.rs
+++ crates/day1/src/main.rs
@@ -1,30 +1,34 @@
 fn main() {
     let input = include_str!("input.txt");
 
-    println!("sum: {}", lines_sum(input));
+    println!("sum (simple): {}", lines_sum_simple(input));
 }
 
-fn lines_sum(input: &str) -> u32 {
-    input
-        .lines()
-        .map(|line| {
-            let digits = line
-                .chars()
-                .filter_map(|c| c.to_digit(10))
-                .collect::<Vec<_>>();
-            digits[0] * 10 + digits[digits.len() - 1]
-        })
-        .sum()
+fn lines_sum_simple(input: &str) -> u32 {
+    lines_sum(input, |line| {
+        let digits = line
+            .chars()
+            .filter_map(|c| c.to_digit(10))
+            .collect::<Vec<_>>();
+
+        digits[0] * 10 + digits[digits.len() - 1]
+    })
 }
 
-#[cfg(test)]
-const TEST_INPUT: &str = "1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet";
+fn lines_sum(input: &str, digits: impl Fn(&str) -> u32) -> u32 {
+    input.lines().map(digits).sum()
+}
 
 #[test]
 fn part1() {
-    assert_eq!(lines_sum("1122"), 12);
-    assert_eq!(lines_sum(TEST_INPUT), 142);
+    assert_eq!(lines_sum_simple("1122"), 12);
+    assert_eq!(
+        lines_sum_simple(
+            "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"
+        ),
+        142
+    );
 }
