8a8724d2a2a5d7a1e6c9c8522a0db343437c39c6
Maarten Staa <maarten@staa.dev>
2023-12-06 10:04:27 +0100

    feat: day 6, part 1

---
+++ crates/day6/src/main.rs
@@ -0,0 +1,70 @@
+fn main() {
+    let input = include_str!("input.txt");
+    let races = parse(input);
+
+    println!(
+        "Product of number of ways to win: {}",
+        races.iter().map(count_ways_to_win).product::<usize>()
+    );
+}
+
+struct Race {
+    time: u32,
+    distance: u32,
+}
+
+fn count_ways_to_win(race: &Race) -> usize {
+    for button_press_time in 1..race.time {
+        let speed = button_press_time;
+        let remaining_time = race.time - button_press_time;
+        let distance_traveled = speed * remaining_time;
+
+        // The first time we encounter this, we know enough to determine the
+        // total number of ways to win, as it's symmetrical.
+        if distance_traveled > race.distance {
+            return (button_press_time..=(race.time - button_press_time)).count();
+        }
+    }
+
+    0
+}
+
+fn parse(input: &str) -> Vec<Race> {
+    let mut lines = input.lines();
+    let times = lines
+        .next()
+        .unwrap()
+        .strip_prefix("Time:")
+        .unwrap()
+        .trim()
+        .split_ascii_whitespace()
+        .map(|s| s.parse().unwrap());
+    let mut distances = lines
+        .next()
+        .unwrap()
+        .strip_prefix("Distance:")
+        .unwrap()
+        .trim()
+        .split_ascii_whitespace()
+        .map(|s| s.parse().unwrap());
+    assert_eq!(lines.next(), None);
+
+    times
+        .map(|time| Race {
+            time,
+            distance: distances.next().unwrap(),
+        })
+        .collect()
+}
+
+#[cfg(test)]
+const TEST_INPUT: &str = "Time:      7  15   30
+Distance:  9  40  200";
+
+#[test]
+fn day6_part1() {
+    let races = parse(TEST_INPUT);
+
+    assert_eq!(count_ways_to_win(&races[0]), 4);
+    assert_eq!(races.iter().map(count_ways_to_win).product::<usize>(), 288);
+}
