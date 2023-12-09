c6abcfa5d8af927d6ecc84b7417b7bd87d0d2e59
Maarten Staa <maarten@staa.dev>
2023-12-09 17:25:34 +0100

    feat: day 9, part 2

--- crates/day9/src/main.rs
+++ crates/day9/src/main.rs
@@ -2,7 +2,8 @@ fn main() {
     let input = include_str!("input.txt");
     let input = parse(input);
 
-    println!("Sum of predictions: {}", part1(&input));
+    println!("Sum of next values: {}", part1(&input));
+    println!("Sum of previous values: {}", part2(&input));
 }
 
 fn parse(input: &str) -> Vec<Vec<i64>> {
@@ -17,19 +18,40 @@ fn parse(input: &str) -> Vec<Vec<i64>> {
 }
 
 fn part1(input: &[Vec<i64>]) -> i64 {
-    input.iter().map(|row| predict_next_number(row)).sum()
+    input
+        .iter()
+        .map(|row| predict_number(row, PredictionMode::Next))
+        .sum()
 }
 
-fn predict_next_number(row: &[i64]) -> i64 {
+fn part2(input: &[Vec<i64>]) -> i64 {
+    input
+        .iter()
+        .map(|row| predict_number(row, PredictionMode::Previous))
+        .sum()
+}
+
+enum PredictionMode {
+    Next,
+    Previous,
+}
+
+fn predict_number(row: &[i64], mode: PredictionMode) -> i64 {
     let differences = row
         .windows(2)
         .map(|pair| pair[1] - pair[0])
         .collect::<Vec<_>>();
 
     if differences.iter().all(|n| *n == 0) {
-        row[row.len() - 1]
+        row[match mode {
+            PredictionMode::Next => row.len() - 1,
+            PredictionMode::Previous => 0,
+        }]
     } else {
-        row[row.len() - 1] + predict_next_number(&differences)
+        match mode {
+            PredictionMode::Next => row[row.len() - 1] + predict_number(&differences, mode),
+            PredictionMode::Previous => row[0] - predict_number(&differences, mode),
+        }
     }
 }
 
@@ -42,4 +64,10 @@ const TEST_INPUT: &str = "0 3 6 9 12 15
 fn day9_part1() {
     let input = parse(TEST_INPUT);
     assert_eq!(part1(&input), 114);
+}
+
+#[test]
+fn day9_part2() {
+    let input = parse(TEST_INPUT);
+    assert_eq!(part2(&input), 2);
 }
