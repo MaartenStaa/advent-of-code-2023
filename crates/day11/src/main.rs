258c1efbb2f744f2c6b20520f3ad4ef9f847a0f2
Maarten Staa <maarten@staa.dev>
2023-12-11 10:38:57 +0100

    feat: day 11, part 1

---
+++ crates/day11/src/main.rs
@@ -0,0 +1,83 @@
+fn main() {
+    let input = include_str!("input.txt");
+    let galaxies = parse(input);
+
+    println!(
+        "Shortest distance pairs sum: {}",
+        shortest_distance_pairs_sum(&galaxies)
+    );
+}
+
+#[derive(Debug, PartialEq)]
+struct Galaxy {
+    position: (usize, usize),
+}
+
+impl Galaxy {
+    fn distance(&self, other: &Galaxy) -> usize {
+        ((self.position.0 as isize - other.position.0 as isize).abs() - 1
+            + (self.position.1 as isize - other.position.1 as isize).abs()
+            + 1) as usize
+    }
+}
+
+#[allow(dead_code)]
+fn parse(input: &str) -> Vec<Galaxy> {
+    let mut rows_with_galaxies = Vec::new();
+    let mut columns_with_galaxies = Vec::new();
+    let mut galaxies = Vec::new();
+
+    for (y, line) in input.lines().enumerate() {
+        for (x, c) in line.chars().enumerate() {
+            if c == '#' {
+                galaxies.push(Galaxy { position: (x, y) });
+                rows_with_galaxies.push(y);
+                columns_with_galaxies.push(x);
+            }
+        }
+    }
+
+    // Apply expansion
+    for galaxy in &mut galaxies {
+        galaxy.position.0 += (0..galaxy.position.0)
+            .filter(|x| !columns_with_galaxies.contains(x))
+            .count();
+        galaxy.position.1 += (0..galaxy.position.1)
+            .filter(|y| !rows_with_galaxies.contains(y))
+            .count();
+    }
+
+    galaxies
+}
+
+fn shortest_distance_pairs_sum(galaxies: &[Galaxy]) -> usize {
+    let mut sum = 0;
+    for (i, galaxy) in galaxies.iter().enumerate() {
+        for other_galaxy in &galaxies[i + 1..] {
+            sum += galaxy.distance(other_galaxy);
+        }
+    }
+    sum
+}
+
+#[cfg(test)]
+const TEST_INPUT: &str = "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....";
+
+#[test]
+fn day11_part1() {
+    let galaxies = parse(TEST_INPUT);
+    assert_eq!(galaxies.len(), 9);
+    assert_eq!(galaxies[0].position, (4, 0));
+    assert_eq!(galaxies[0].distance(&galaxies[6]), 15);
+
+    assert_eq!(shortest_distance_pairs_sum(&galaxies), 374);
+}
