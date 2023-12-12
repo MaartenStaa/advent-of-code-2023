21223ea3fefc68768c593740481546b8aacd2307
Maarten Staa <maarten@staa.dev>
2023-12-12 17:34:27 +0100

    feat: day 12, part 2

--- crates/day12/src/main.rs
+++ crates/day12/src/main.rs
@@ -1,12 +1,28 @@
+use std::collections::HashMap;
+
+use rayon::prelude::*;
+
 fn main() {
     let input = include_str!("input.txt");
-    let records = parse(input);
+    let mut records = parse(input);
 
     println!(
         "Number of possible states: {}",
         records
             .iter()
-            .map(|record| record.get_possible_states().len())
+            .map(|record| record.get_possible_states_count())
+            .sum::<usize>()
+    );
+
+    for record in records.iter_mut() {
+        record.multiply(5);
+    }
+
+    println!(
+        "Number of possible states with more expansion: {}",
+        records
+            .par_iter()
+            .map(|record| record.get_possible_states_count())
             .sum::<usize>()
     );
 }
@@ -15,7 +31,7 @@ fn parse(input: &str) -> Vec<Record> {
     input.lines().map(Record::parse).collect()
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 enum ParsedState {
     Operational,
     Damaged,
@@ -46,96 +62,112 @@ impl Record {
         }
     }
 
-    fn get_possible_states(&self) -> Vec<Vec<State>> {
-        #[derive(Debug, Clone)]
-        struct PossibleState {
-            states: Vec<State>,
-            damaged_groups: Vec<usize>,
-            previous_was_damaged: bool,
-        }
-
-        let mut possible_states = vec![PossibleState {
-            states: Vec::with_capacity(self.states.len()),
-            damaged_groups: vec![],
-            previous_was_damaged: false,
-        }];
-        for s in &self.states {
-            dbg!(s);
-            match s {
-                ParsedState::Operational => possible_states.iter_mut().for_each(|state| {
-                    if state.previous_was_damaged {
-                        state.previous_was_damaged = false;
-                    }
-                    state.states.push(State::Operational);
-                }),
-                ParsedState::Damaged => possible_states.iter_mut().for_each(|state| {
-                    if state.previous_was_damaged {
-                        let index = state.damaged_groups.len() - 1;
-                        state.damaged_groups[index] += 1;
-                    } else {
-                        state.damaged_groups.push(1);
-                        state.previous_was_damaged = true;
-                    }
-                    state.states.push(State::Damaged);
-                }),
-                ParsedState::Unknown => {
-                    // Branch
-                    let damaged_state = possible_states.clone();
-
-                    // Always safe to add an operational state
-                    possible_states.iter_mut().for_each(|state| {
-                        if state.previous_was_damaged {
-                            state.previous_was_damaged = false;
-                        }
-                        state.states.push(State::Operational);
-                    });
-
-                    // Add a damaged state to the damaged state branch, but only if:
-                    // - It would not exceed the contigious damaged count
-                    // - We have not yet exceeded the blocks of contigious damaged counts
-                    for mut state in damaged_state.into_iter().filter(|state| {
-                        if state.previous_was_damaged {
-                            state.damaged_groups.len() <= self.contigious_damaged_counts.len()
-                                && state.damaged_groups[state.damaged_groups.len() - 1]
-                                    < self.contigious_damaged_counts[state.damaged_groups.len() - 1]
-                        } else {
-                            // Need to start a new group, so we need to make sure we have not
-                            // exceeded the number of groups
-                            state.damaged_groups.len() < self.contigious_damaged_counts.len()
-                        }
-                    }) {
-                        if state.previous_was_damaged {
-                            let index = state.damaged_groups.len() - 1;
-                            state.damaged_groups[index] += 1;
-                        } else {
-                            state.damaged_groups.push(1);
-                            state.previous_was_damaged = true;
-                        }
-                        state.states.push(State::Damaged);
-
-                        possible_states.push(state);
-                    }
-                }
+    fn multiply(&mut self, n: usize) {
+        self.states = (0..n).fold(vec![], |mut acc, i| {
+            acc.extend(self.states.iter().cloned());
+            if i < n - 1 {
+                acc.push(ParsedState::Unknown);
             }
-        }
+            acc
+        });
+        self.contigious_damaged_counts = self
+            .contigious_damaged_counts
+            .iter()
+            .cycle()
+            .take(self.contigious_damaged_counts.len() * n)
+            .copied()
+            .collect();
+    }
 
-        possible_states
-            .into_iter()
-            .filter_map(|state| {
-                if state.damaged_groups == self.contigious_damaged_counts {
-                    Some(state.states)
+    fn get_possible_states_count(&self) -> usize {
+        fn get_possible_states_count_2_inner(
+            cached_results: &mut HashMap<(Vec<ParsedState>, Vec<usize>, bool), usize>,
+            states: &[ParsedState],
+            contigious_damaged_counts: &[usize],
+            previous_was_damaged: bool,
+            damaged_groups: &[usize],
+        ) -> usize {
+            let key = (
+                states.to_vec(),
+                damaged_groups.to_vec(),
+                previous_was_damaged,
+            );
+            if let Some(result) = cached_results.get(&key) {
+                return *result;
+            }
+
+            let mut result = states.is_empty().then(|| {
+                if damaged_groups == contigious_damaged_counts {
+                    1
+                } else {
+                    0
+                }
+            });
+
+            result = result.or_else(|| {
+                if damaged_groups.len() > contigious_damaged_counts.len()
+                    || (!previous_was_damaged
+                        && damaged_groups != &contigious_damaged_counts[..damaged_groups.len()])
+                {
+                    Some(0)
                 } else {
                     None
                 }
-            })
-            .collect()
-    }
-}
+            });
 
-#[derive(Debug, Clone, PartialEq)]
-enum State {
-    Operational,
-    Damaged,
+            let result = result.unwrap_or_else(|| {
+                let mut result = 0;
+                if matches!(states[0], ParsedState::Operational | ParsedState::Unknown) {
+                    result += get_possible_states_count_2_inner(
+                        cached_results,
+                        &states[1..],
+                        contigious_damaged_counts,
+                        false,
+                        damaged_groups,
+                    );
+                }
+
+                // No else if, allow unknown to enter both clauses
+                if matches!(states[0], ParsedState::Damaged | ParsedState::Unknown) {
+                    let mut damaged_groups = damaged_groups.to_vec();
+
+                    result += if previous_was_damaged {
+                        let index = damaged_groups.len() - 1;
+                        damaged_groups[index] += 1;
+                        get_possible_states_count_2_inner(
+                            cached_results,
+                            &states[1..],
+                            contigious_damaged_counts,
+                            true,
+                            &damaged_groups,
+                        )
+                    } else {
+                        damaged_groups.push(1);
+                        get_possible_states_count_2_inner(
+                            cached_results,
+                            &states[1..],
+                            contigious_damaged_counts,
+                            true,
+                            &damaged_groups,
+                        )
+                    };
+                }
+
+                result
+            });
+
+            cached_results.insert(key, result);
+            result
+        }
+
+        get_possible_states_count_2_inner(
+            &mut HashMap::new(),
+            &self.states,
+            &self.contigious_damaged_counts,
+            false,
+            &[],
+        )
+    }
 }
 
 #[test]
@@ -149,10 +181,32 @@ fn day12_part1() {
 ?###???????? 3,2,1",
     );
     assert_eq!(records.len(), 6);
-    assert_eq!(records[0].get_possible_states().len(), 1);
-    assert_eq!(records[1].get_possible_states().len(), 4);
-    assert_eq!(records[2].get_possible_states().len(), 1);
-    assert_eq!(records[3].get_possible_states().len(), 1);
-    assert_eq!(records[4].get_possible_states().len(), 4);
-    assert_eq!(records[5].get_possible_states().len(), 10);
+    assert_eq!(records[0].get_possible_states_count(), 1);
+    assert_eq!(records[1].get_possible_states_count(), 4);
+    assert_eq!(records[2].get_possible_states_count(), 1);
+    assert_eq!(records[3].get_possible_states_count(), 1);
+    assert_eq!(records[4].get_possible_states_count(), 4);
+    assert_eq!(records[5].get_possible_states_count(), 10);
+}
+
+#[test]
+fn day12_part2() {
+    let mut records = parse(
+        "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1",
+    );
+    for record in records.iter_mut() {
+        record.multiply(5);
+    }
+
+    assert_eq!(records[0].get_possible_states_count(), 1);
+    assert_eq!(records[1].get_possible_states_count(), 16384);
+    assert_eq!(records[2].get_possible_states_count(), 1);
+    assert_eq!(records[3].get_possible_states_count(), 16);
+    assert_eq!(records[4].get_possible_states_count(), 2500);
+    assert_eq!(records[5].get_possible_states_count(), 506250);
 }
