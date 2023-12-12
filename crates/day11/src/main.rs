6f9cf8d2d6f76b8d81105ecbdd0afafe8e09b7e0
Maarten Staa <maarten@staa.dev>
2023-12-12 11:11:55 +0100

    feat: day 11, part 2

--- crates/day11/src/main.rs
+++ crates/day11/src/main.rs
@@ -1,9 +1,15 @@
 fn main() {
     let input = include_str!("input.txt");
-    let galaxies = parse(input);
+    let galaxies = parse(input, 2);
 
     println!(
         "Shortest distance pairs sum: {}",
+        shortest_distance_pairs_sum(&galaxies)
+    );
+
+    let galaxies = parse(input, 1000000);
+    println!(
+        "Shortest distance pairs sum with more expansion: {}",
         shortest_distance_pairs_sum(&galaxies)
     );
 }
@@ -22,7 +28,7 @@ impl Galaxy {
 }
 
 #[allow(dead_code)]
-fn parse(input: &str) -> Vec<Galaxy> {
+fn parse(input: &str, expansion_ratio: usize) -> Vec<Galaxy> {
     let mut rows_with_galaxies = Vec::new();
     let mut columns_with_galaxies = Vec::new();
     let mut galaxies = Vec::new();
@@ -41,10 +47,12 @@ fn parse(input: &str) -> Vec<Galaxy> {
     for galaxy in &mut galaxies {
         galaxy.position.0 += (0..galaxy.position.0)
             .filter(|x| !columns_with_galaxies.contains(x))
-            .count();
+            .count()
+            * (expansion_ratio - 1);
         galaxy.position.1 += (0..galaxy.position.1)
             .filter(|y| !rows_with_galaxies.contains(y))
-            .count();
+            .count()
+            * (expansion_ratio - 1);
     }
 
     galaxies
@@ -74,10 +82,19 @@ const TEST_INPUT: &str = "...#......
 
 #[test]
 fn day11_part1() {
-    let galaxies = parse(TEST_INPUT);
+    let galaxies = parse(TEST_INPUT, 2);
     assert_eq!(galaxies.len(), 9);
     assert_eq!(galaxies[0].position, (4, 0));
     assert_eq!(galaxies[0].distance(&galaxies[6]), 15);
 
     assert_eq!(shortest_distance_pairs_sum(&galaxies), 374);
+}
+
+#[test]
+fn day11_part2() {
+    let galaxies = parse(TEST_INPUT, 10);
+    assert_eq!(shortest_distance_pairs_sum(&galaxies), 1030);
+
+    let galaxies = parse(TEST_INPUT, 100);
+    assert_eq!(shortest_distance_pairs_sum(&galaxies), 8410);
 }
