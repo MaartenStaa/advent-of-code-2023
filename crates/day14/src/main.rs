ee1adfd8c380afbad1b3b886833c27d825da82cf
Maarten Staa <maarten@staa.dev>
2023-12-14 11:46:24 +0100

    feat: day 14, part 2

--- crates/day14/src/main.rs
+++ crates/day14/src/main.rs
@@ -1,12 +1,18 @@
+use std::collections::HashMap;
+
 fn main() {
     let input = include_str!("input.txt");
     let mut grid = parse(input);
     grid.roll_round_stones_north();
 
     println!("Part 1: {}", grid.total_load());
+
+    let mut grid = parse(input);
+    grid.spin_cycle(1000000000);
+    println!("Part 2: {}", grid.total_load());
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 enum Cell {
     Empty,
     RoundStone,
@@ -28,6 +34,10 @@ impl Grid {
 
             let x = i % self.width;
             let y = i / self.width;
+            if y == 0 {
+                continue;
+            }
+
             let mut target_index = None;
 
             for offset in 1..=y {
@@ -43,6 +53,122 @@ impl Grid {
             if let Some(target_index) = target_index {
                 self.cells.swap(i, target_index);
             }
+        }
+    }
+
+    fn roll_round_stones_east(&mut self) {
+        for i_from_end in 1..=self.cells.len() {
+            let i = self.cells.len() - i_from_end;
+            if self.cells[i] != Cell::RoundStone {
+                continue;
+            }
+
+            let x = i % self.width;
+            if x == self.width - 1 {
+                continue;
+            }
+
+            let y = i / self.width;
+            let mut target_index = None;
+
+            for offset in 1..(self.width - x) {
+                let index = x + offset + y * self.width;
+                match self.cells[index] {
+                    Cell::Empty => {
+                        target_index = Some(index);
+                    }
+                    Cell::RoundStone | Cell::SquareStone => break,
+                }
+            }
+
+            if let Some(target_index) = target_index {
+                self.cells.swap(i, target_index);
+            }
+        }
+    }
+
+    fn roll_round_stones_south(&mut self) {
+        let height = self.cells.len() / self.width;
+        for i_from_end in 1..=self.cells.len() {
+            let i = self.cells.len() - i_from_end;
+            if self.cells[i] != Cell::RoundStone {
+                continue;
+            }
+
+            let x = i % self.width;
+            let y = i / self.width;
+            if y == height - 1 {
+                continue;
+            }
+
+            let mut target_index = None;
+
+            for offset in 1..(height - y) {
+                let index = x + (y + offset) * self.width;
+                match self.cells[index] {
+                    Cell::Empty => {
+                        target_index = Some(index);
+                    }
+                    Cell::RoundStone | Cell::SquareStone => break,
+                }
+            }
+
+            if let Some(target_index) = target_index {
+                self.cells.swap(i, target_index);
+            }
+        }
+    }
+
+    fn roll_round_stones_west(&mut self) {
+        for i in 0..self.cells.len() {
+            if self.cells[i] != Cell::RoundStone {
+                continue;
+            }
+
+            let x = i % self.width;
+            if x == 0 {
+                continue;
+            }
+
+            let y = i / self.width;
+            let mut target_index = None;
+
+            for offset in 1..=x {
+                let index = x - offset + y * self.width;
+                match self.cells[index] {
+                    Cell::Empty => {
+                        target_index = Some(index);
+                    }
+                    Cell::RoundStone | Cell::SquareStone => break,
+                }
+            }
+
+            if let Some(target_index) = target_index {
+                self.cells.swap(i, target_index);
+            }
+        }
+    }
+
+    fn spin_cycle(&mut self, times: usize) {
+        let mut cache = HashMap::new();
+        for i in 0..times {
+            if let Some(previous_i) = cache.insert(self.cells.clone(), i) {
+                let cycle = i - previous_i;
+                let remaining = (times - i) % cycle;
+                for _ in 0..remaining {
+                    self.roll_round_stones_north();
+                    self.roll_round_stones_west();
+                    self.roll_round_stones_south();
+                    self.roll_round_stones_east();
+                }
+
+                break;
+            }
+
+            self.roll_round_stones_north();
+            self.roll_round_stones_west();
+            self.roll_round_stones_south();
+            self.roll_round_stones_east();
         }
     }
 
@@ -105,4 +231,12 @@ fn day14_part1() {
     grid.roll_round_stones_north();
 
     assert_eq!(grid.total_load(), 136);
+}
+
+#[test]
+fn day14_part2() {
+    let mut grid = parse(TEST_INPUT);
+    grid.spin_cycle(1000000000);
+
+    assert_eq!(grid.total_load(), 64);
 }
